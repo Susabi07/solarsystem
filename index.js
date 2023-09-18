311c3c1d4f7f9942d8abc14efdf61540652c3dfe
Thompson Harris <william.thompson.harris@gmail.com>
2023-09-18 18:25:27 -0400

    updated style system and theme changing functionlity

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-document.querySelector('.planetWrapperJupiter').addEventListener('click', function() {
-    console.log('YOU CLICKED JUPITER');
+const theme = ['purpleTheme', 'altTheme', 'greenTheme']
+
+const body = document.querySelector('body');
+
+document.querySelector('#theme').addEventListener('click', function() {
+    const currentThemeIndex = theme.indexOf(body.className);
+    const nextThemeIndex = (currentThemeIndex + 1) % theme.length;
+    body.className = theme[nextThemeIndex];
 });
